9995a9914b85e0d65d260ab354f3520a2b81e43b
Arturas Lebedevas <nsmeta@gmail.com>
2013-10-30 15:32:54 +0000

    [hooks] "exists" - tests

--- test/exploration_test.js
+++ test/exploration_test.js
@@ -30,8 +30,23 @@ var tNodeProject = geny(function (projectName, data) {
   });
 });
 
+var tOnExists = geny(function (onExists) {
+
+  this.on('exists', function (path) {
+    return onExists;
+  });
+
+  this.dir('on_exists', function () {
+    this.file('hello.txt', {
+      template: fs.readFileSync(ROOT_DIR + 'templates/human.txt.hbs').toString(),
+      data: { name: 'Artur' }
+    });
+  });
+});
+
 // Plain Template
 var tGreeter = geny(function (data) {
+
   this.file('zoo/human/msg.txt', {
     template: fs.readFileSync(ROOT_DIR + 'templates/human.txt.hbs').toString(),
     data: data
@@ -116,4 +131,41 @@ describe('geny.js', function () {
   });
 
   it('should create empty file, if no template was specified');
+
+  describe('@on', function () {
+    describe('"exists"', function () {
+      it('should stop geneation when file already exists', function () {
+        var test_file_path = ROOT_DIR + 'hello.txt',
+            test_file_data = 'Hello!',
+            data;
+
+        tOnExists.create(ROOT_DIR);
+        fs.writeFileSync(test_file_path, test_file_data);
+
+        // Don't allow to continue
+        tOnExists.create(ROOT_DIR, false);
+        data = fs.readFileSync(test_file_path).toString();
+        data.should.eql(test_file_data);
+
+        // Clean up
+        tOnExists.destroy(ROOT_DIR);
+      });
+      it('should continue when file already exists', function () {
+        var test_file_path = ROOT_DIR + 'hello.txt',
+            test_file_data = 'Hello!',
+            data;
+
+        tOnExists.create(ROOT_DIR);
+        fs.writeFileSync(test_file_path, test_file_data);
+
+        // Allow to continue
+        tOnExists.create(ROOT_DIR, true);
+        data = fs.readFileSync(test_file_path).toString();
+        data.should.eql("Greetings, Artur!");
+
+        // Clean up
+        tOnExists.destroy(ROOT_DIR);
+      });
+    });
+  });
 });
